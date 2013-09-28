dabd4f44f00b11bbf942aafbf5d5b7529b767b4e
default2 <default2@192.168.11.6>
2013-09-28 21:27:56 +0900

    BOT-1設定画面作成中

--- src/kinreiboku/tomorrow/botter/MainActivity.java
+++ src/kinreiboku/tomorrow/botter/MainActivity.java
@@ -1,15 +1,23 @@
 package kinreiboku.tomorrow.botter;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.app.Activity;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceClickListener;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceScreen;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+public class MainActivity extends PreferenceActivity implements OnPreferenceClickListener{
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+	    // PreferenceScreenからのIntent
+	    PreferenceScreen nextMove1 = (PreferenceScreen) this.findPreference(this.getString(R.string.screen_key));
+	    nextMove1.setOnPreferenceClickListener(this);
 	}
 
 	@Override
@@ -17,6 +25,17 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	@Override
+	public boolean onPreferenceClick(Preference preference) {
+        // Activityの遷移
+        Intent nextActivity = new Intent(
+        		MainActivity.this,
+                TweetEditListActivity.class);
+
+        startActivity(nextActivity);
+        return true;
 	}
 
 }
